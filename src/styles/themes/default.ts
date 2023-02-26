c78ff274271310e659f18e98d4b25b0d805f3cc7
Maycon Silva <mayconslv.contato@gmail.com>
2023-02-26 14:41:06 -0300

    Setup inicial

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-700': '#121214',
+  'grey-600': '#202024',
+  'gray-500': '#29292e',
+  'gray-400': '#323238',
+  'gray-300': '#7C7C8A',
+  'gray-200': '#C4C4CC',
+  'gray-100': '#E1E1E6',
+
+  'base-green': '#00875f',
+  'green-dark': '#015F43',
+  'green-light': '#00B37E',
+
+  'base-red': '#F75A68',
+  'red-dark': '#AA2834',
+} as const

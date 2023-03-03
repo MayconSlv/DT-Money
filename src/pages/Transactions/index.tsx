5df0c44e4ba40464288faf547afa7091a89f5d64
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-03 20:09:01 -0300

    Criando Hook para o Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -12,26 +12,11 @@ import { SearchForm } from './components'
 import { useContext } from 'react'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function TransactionsPage() {
   const { transactions } = useContext(TransactionsContext)
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   const colors = useTheme()
   return (
